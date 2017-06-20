780ab28c5d7d99b7efdc5de7b6f5dca5dbf02fa1
codericeps <codericeps@gmail.com>
2017-06-20 16:33:54 +0300

    create static stub data

--- app/src/main/java/com/example/user/jakewhartonrepos/domain/datasource/MockGithubDataSource.kt
+++ app/src/main/java/com/example/user/jakewhartonrepos/domain/datasource/MockGithubDataSource.kt
@@ -1,19 +1,19 @@
 package com.example.user.jakewhartonrepos.domain.datasource
 
-import com.example.user.jakewhartonrepos.domain.GitDataRepository
 import com.example.user.jakewhartonrepos.model.GithubRepositoryModel
 import io.reactivex.Observable
 
 /**
  * Created by User on 20.06.2017.
  */
-class MockGithubDataSource: GithubRepositoriesSource {
+class MockGithubDataSource : GithubRepositoriesSource {
 
     val whartonsRepos: ArrayList<GithubRepositoryModel> = ArrayList()
+
     init {
-        whartonsRepos.add(GithubRepositoryModel("timber", "Jake Wharton", 4217))
-        whartonsRepos.add(GithubRepositoryModel("kotterknife", "Jake Wharton", 1278))
-        whartonsRepos.add(GithubRepositoryModel("Reagent", "Jake Wharton", 74))
+        whartonsRepos.add(GithubRepositoryModel("timber", "A logger with a small, extensible API which provides utility on top of Android's normal Log class.", 4217))
+        whartonsRepos.add(GithubRepositoryModel("kotterknife", "View injection library for Android.", 1278))
+        whartonsRepos.add(GithubRepositoryModel("Reagent", "An experiment which rewrites RxJava-like types using polymorphism (and also in Kotlin).", 74))
     }
 
     override fun getGithubRepositories(): Observable<GithubRepositoryModel> {
