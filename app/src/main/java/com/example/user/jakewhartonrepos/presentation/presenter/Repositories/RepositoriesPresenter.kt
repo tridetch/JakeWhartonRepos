ed294c2ebd2641fc8415e2bb3d1ec6442af6505b
codericeps <codericeps@gmail.com>
2017-06-20 18:11:36 +0300

    use Rx for fetching and filter data

--- app/src/main/java/com/example/user/jakewhartonrepos/presentation/presenter/Repositories/RepositoriesPresenter.kt
+++ app/src/main/java/com/example/user/jakewhartonrepos/presentation/presenter/Repositories/RepositoriesPresenter.kt
@@ -2,9 +2,18 @@ package com.example.user.jakewhartonrepos.presentation.presenter.Repositories
 
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
+import com.example.user.jakewhartonrepos.domain.GitDataRepository
 import com.example.user.jakewhartonrepos.presentation.view.Repositories.RepositoriesView
 
 @InjectViewState
-class RepositoriesPresenter : MvpPresenter<RepositoriesView>() {
+class RepositoriesPresenter(val githubDataRepository: GitDataRepository) : MvpPresenter<RepositoriesView>() {
+
+    fun onAttach() {
+        githubDataRepository.getGithubRepositories().filter { r-> !r.name.startsWith("T") }.subscribe(
+                {gitHubRepo -> viewState.showRepoInList(gitHubRepo)},
+                {err -> viewState.showErrorMessage()},
+                {-> viewState.showCompleteMessage()}
+        )
+    }
 
 }
