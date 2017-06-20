5b55e25f1180f4dcb2e0d1da4b523fd507e450d0
codericeps <codericeps@gmail.com>
2017-06-20 15:07:58 +0300

    add simple data repository and mock datasource

---
+++ app/src/main/java/com/example/user/jakewhartonrepos/domain/datasource/MockGithubDataSource.kt
@@ -0,0 +1,27 @@
+package com.example.user.jakewhartonrepos.domain.datasource
+
+import com.example.user.jakewhartonrepos.domain.GitDataRepository
+import com.example.user.jakewhartonrepos.model.GithubRepositoryModel
+import io.reactivex.Observable
+
+/**
+ * Created by User on 20.06.2017.
+ */
+class MockGithubDataSource: GitDataRepository {
+
+    val whartonsRepos: ArrayList<GithubRepositoryModel> = ArrayList()
+    init {
+        whartonsRepos.add(GithubRepositoryModel("timber", "Jake Wharton", 4217))
+        whartonsRepos.add(GithubRepositoryModel("kotterknife", "Jake Wharton", 1278))
+        whartonsRepos.add(GithubRepositoryModel("Reagent", "Jake Wharton", 74))
+    }
+
+    override fun getGithubRepositories(): Observable<GithubRepositoryModel> {
+        return Observable.create { emitter ->
+            for (repository in whartonsRepos) {
+                emitter.onNext(repository)
+            }
+            emitter.onComplete()
+        }
+    }
+}
