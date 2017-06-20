58487b4213cf9c5b42a97f372159a1141b265228
codericeps <codericeps@gmail.com>
2017-06-20 18:09:57 +0300

    create view methods

--- app/src/main/java/com/example/user/jakewhartonrepos/presentation/view/Repositories/RepositoriesView.kt
+++ app/src/main/java/com/example/user/jakewhartonrepos/presentation/view/Repositories/RepositoriesView.kt
@@ -1,7 +1,10 @@
 package com.example.user.jakewhartonrepos.presentation.view.Repositories
 
 import com.arellomobile.mvp.MvpView
+import com.example.user.jakewhartonrepos.model.GithubRepositoryModel
 
 interface RepositoriesView : MvpView {
-
+    fun showRepoInList(gitHubRepo: GithubRepositoryModel)
+    fun showErrorMessage()
+    fun showCompleteMessage()
 }
