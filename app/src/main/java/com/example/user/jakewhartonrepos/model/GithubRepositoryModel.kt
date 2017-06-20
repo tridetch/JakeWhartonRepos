bc7d1550989333c5fcb9f669fbd979a60f4b5c44
codericeps <codericeps@gmail.com>
2017-06-20 14:37:05 +0300

    simple model for github repos

---
+++ app/src/main/java/com/example/user/jakewhartonrepos/model/GithubRepositoryModel.kt
@@ -0,0 +1,8 @@
+package com.example.user.jakewhartonrepos.model
+
+/**
+ * Created by User on 20.06.2017.
+ */
+data class GithubRepositoryModel(val name: String, val owner: String, val stars: Int) {
+
+}
