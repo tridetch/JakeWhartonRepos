55118fadc31d8eddc3059172e79365025aa20a8e
codericeps <codericeps@gmail.com>
2017-06-20 16:31:48 +0300

    remove owner prop

--- app/src/main/java/com/example/user/jakewhartonrepos/model/GithubRepositoryModel.kt
+++ app/src/main/java/com/example/user/jakewhartonrepos/model/GithubRepositoryModel.kt
@@ -3,6 +3,6 @@ package com.example.user.jakewhartonrepos.model
 /**
  * Created by User on 20.06.2017.
  */
-data class GithubRepositoryModel(val name: String, val owner: String, val stars: Int) {
+data class GithubRepositoryModel(val name: String, val description: String, val stars: Int) {
 
 }
