d011216bd01b4edd13965eb6b61dd8aafdc1a3fc
codericeps <codericeps@gmail.com>
2017-06-20 19:15:48 +0300

    ad todo

--- app/src/main/java/com/example/user/jakewhartonrepos/presentation/presenter/Repositories/RepositoriesPresenter.kt
+++ app/src/main/java/com/example/user/jakewhartonrepos/presentation/presenter/Repositories/RepositoriesPresenter.kt
@@ -4,15 +4,17 @@ import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.example.user.jakewhartonrepos.domain.GitDataRepository
 import com.example.user.jakewhartonrepos.presentation.view.Repositories.RepositoriesView
+import io.reactivex.schedulers.Schedulers
 
 @InjectViewState
 class RepositoriesPresenter(val githubDataRepository: GitDataRepository) : MvpPresenter<RepositoriesView>() {
 
     fun onAttach() {
-        githubDataRepository.getGithubRepositories().filter { r-> !r.name.startsWith("T") }.subscribe(
-                {gitHubRepo -> viewState.showRepoInList(gitHubRepo)},
-                {err -> viewState.showErrorMessage()},
-                {-> viewState.showCompleteMessage()}
+//        TODO("later") // observe on backgroundThread, subscribe on main thread
+        githubDataRepository.getGithubRepositories().filter{ r -> !r.name.startsWith("T",true) }.subscribe(
+                { gitHubRepo -> viewState.showRepoInList(gitHubRepo) },
+                { err -> viewState.showErrorMessage() },
+                { -> viewState.showCompleteMessage() }
         )
     }
 
